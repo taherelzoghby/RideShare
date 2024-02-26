83a68ecf04b6fe293e54e63af7b87a675acc941f
taherelzoghby <taheramin442@gmail.com>
2024-02-26 19:57:14 +0200

    added setup for splash

---
+++ lib/features/splash/widgets/splash_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class SplashBody extends StatelessWidget {
+  const SplashBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('splash'),
+      ],
+    );
+  }
+}
