73a92d333ebe8efb59109f47e225fb5c471cb54b
taherelzoghby <taheramin442@gmail.com>
2024-03-02 21:36:47 +0200

    edited routes page

--- lib/core/constants/routesPage.dart
+++ lib/core/constants/routesPage.dart
@@ -1,11 +1,14 @@
 import 'package:go_router/go_router.dart';
 import 'package:ride_share/core/helper/custom_animation.dart';
+import 'package:ride_share/features/auth/presentation/pages/sign_up_view.dart';
 import 'package:ride_share/features/auth/presentation/pages/welcome_view.dart';
 
 import '../../features/boarding/presentation/pages/board_view.dart';
 
 const boardPath = '/';
 const welcomePath = '/welcome';
+const signUpPath = '/signup';
+const loginPath = '/login';
 final router = GoRouter(
   routes: [
     GoRoute(
@@ -22,6 +25,14 @@ final router = GoRouter(
         context: context,
         state: state,
         child: const WelcomeView(),
+      ),
+    ),
+    GoRoute(
+      path: signUpPath,
+      pageBuilder: (context, state) => buildPageWithDefaultTransition(
+        context: context,
+        state: state,
+        child: const SignUpView(),
       ),
     ),
   ],
