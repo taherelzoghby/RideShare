3f79d9e36d55df4532f50a3b8633f42870fdd8e8
taherelzoghby <taheramin442@gmail.com>
2024-03-04 20:44:38 +0200

    added sign in page

---
+++ lib/features/auth/presentation/widgets/sign_in_body.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:ride_share/core/constants/strings.dart';
+import 'package:ride_share/core/constants/style.dart';
+import 'package:ride_share/core/widgets/customButton.dart';
+import 'package:ride_share/features/auth/presentation/widgets/signin_or_signup_with_other_account.dart';
+
+import 'alternaive_auth.dart';
+import 'headline_auth_widget.dart';
+import 'section_fields_sign_in.dart';
+import 'sign_with_other_account.dart';
+
+class SignInBody extends StatelessWidget {
+  const SignInBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: AppConsts.mainPadding,
+      child: Column(
+        children: [
+          const AspectRatio(aspectRatio: AppConsts.aspect16on2),
+          const Align(
+            alignment: Alignment.centerLeft,
+            child: HeadLineAuthSignUp(
+              headline: StringsEn.signInWithEmailOrPhoneNumber,
+            ),
+          ),
+          const AspectRatio(aspectRatio: AppConsts.aspect16on2),
+          const SectionFieldsSignIn(),
+          const Spacer(),
+          AspectRatio(
+            aspectRatio: AppConsts.aspectRatioButtonAuth,
+            child: CustomButton(
+              text: StringsEn.signIn,
+              onTap: () => GoRouter.of(context).push(''),
+            ),
+          ),
+          const OrWidget(),
+          const SignWithGoogleAndFaceBookWidget(),
+          const AlternativeAuth(
+            label: StringsEn.dontHaveAnAccount,
+            trailing: StringsEn.signUp,
+          ),
+        ],
+      ),
+    );
+  }
+}
