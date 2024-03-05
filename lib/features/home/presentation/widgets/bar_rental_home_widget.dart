f131efba901dbeafbf325bfa8a116c81624e4e44
taherelzoghby <taheramin442@gmail.com>
2024-03-05 17:07:44 +0200

    edited rental widget

--- lib/features/home/presentation/widgets/bar_rental_home_widget.dart
+++ lib/features/home/presentation/widgets/bar_rental_home_widget.dart
@@ -4,38 +4,14 @@ import 'package:ride_share/core/constants/style.dart';
 
 import 'bar_component.dart';
 
-class BarRentalHomeWidget extends StatefulWidget {
+class BarRentalHomeWidget extends StatelessWidget {
   const BarRentalHomeWidget({super.key});
 
   @override
-  State<BarRentalHomeWidget> createState() => _BarRentalHomeWidgetState();
-}
-
-class _BarRentalHomeWidgetState extends State<BarRentalHomeWidget> {
-  int active = 0;
-
-  @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: AppConsts.decorationBarRadius15white,
-      child: Row(
-        children: [
-          Expanded(
-            child: BarComponent(
-              title: StringsEn.transport,
-              color: active == 0 ? AppConsts.mainColor2 : null,
-              onTap: () => setState(() => active = 0),
-            ),
-          ),
-          Expanded(
-            child: BarComponent(
-              title: StringsEn.delivery,
-              color: active == 1 ? AppConsts.mainColor2 : null,
-              onTap: () => setState(() => active = 1),
-            ),
-          ),
-        ],
-      ),
+    return BarComponent(
+      title: StringsEn.transport,
+      color: AppConsts.mainColor2,
     );
   }
 }
